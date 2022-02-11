928ed37c87e86fe0945757874dcf10493d4f7ff6
l-khali <97611592+l-khali@users.noreply.github.com>
2022-02-11 14:03:30 +0000

    .

--- fibonacci/fibonacci.py
+++ fibonacci/fibonacci.py
@@ -1,10 +1,18 @@
+from numbers import Integral  
+# integral is adjective form of integer, not a calculus integral
+
+
 def fib(n):
     """Return the n-th Fibonacci number."""
-    if n == 0:
+    if not isinstance(n, Integral):
+        raise TypeError(
+            f"fib expects an integer, not a {type(n).__name__}"
+        )
+    elif n == 0:
         return 0
     elif n == 1:
         return 1
     else:
         return fib(n-2) + fib(n-1)
 
-#useful to have a blank line after the function
+# useful to have a blank line after the function
